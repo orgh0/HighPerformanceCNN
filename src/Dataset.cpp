40bf1ac5dafd8d5ea61d0d787a125328b1b80309
Arghya Bhattacharya <arghya@Arghyas-MacBook-Air.local>
2020-04-24 18:20:19 +0530

    Bug Fix in Dataset

--- src/Dataset.cpp
+++ src/Dataset.cpp
@@ -8,13 +8,18 @@
 #include <fstream>
 #include <random>
 
-Dataset::Dataset(std::string data_path, bool shuffle) : shuffle(shuffle), train_data_index(0), test_data_index(0) {
-    this->read_images(data_path + "/train-images-idx3-ubyte", this->train_data);
-    this->read_labels(data_path + "/train-labels-idx1-ubyte", this->train_label);
-
-    this->read_images(data_path + "/t10k-images-idx3-ubyte", this->test_data);
-    this->read_labels(mnist_data_path + "/t10k-labels-idx1-ubyte", this->test_label);
-
+DataSet::DataSet(std::string data_path, bool shuffle)
+    : shuffle(shuffle), train_data_index(0), test_data_index(0) {
+  // train data
+  this->read_images(data_path + "/train-images-idx3-ubyte",
+                    this->train_data);
+  this->read_labels(data_path + "/train-labels-idx1-ubyte",
+                    this->train_label);
+  // test data
+  this->read_images(data_path + "/t10k-images-idx3-ubyte",
+                    this->test_data);
+  this->read_labels(data_path + "/t10k-labels-idx1-ubyte",
+                    this->test_label);
 }
 
 void Dataset::reset() {
