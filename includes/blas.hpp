b65c121a4f62d33849b649ec96d13c8b0ff469c6
Swapnil Pakhare <swapchamps@gmail.com>
2020-05-04 16:23:56 +0530

    blas for add and subtract added

---
+++ includes/blas.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include<Container.hpp>
+#include<utils.hpp>
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+void blas_add();
+
+void blas_add();
+
+void blas_sub();
---
+++ src/blas.cpp
@@ -0,0 +1,44 @@
+#include<blas.hpp>
+#include<utils.hpp>
+
+#include <cuda_runtime.h>
+#include <thrust/functional.h>
+#include <thrust/transform.h>
+
+#include <algorithm>
+#include <cfloat>
+
+struct add_functor
+{
+    const float e;
+    add_functor(float _e) : e(_e) {}
+    __host__ __device__ float operator()(const float &x) const { return x + e; }
+};
+
+void blas_add(const Container *input1, const Container *input2,
+                  Container *outputs)
+{
+    CHECK_EQ(input1->get_data().size(), input2->get_data().size(),
+             "operator_add: size error");
+
+    thrust::transform(input1->get_data().begin(), input1->get_data().end(),
+                      input2->get_data().begin(), outputs->get_data().begin(),
+                      thrust::plus<float>());
+}
+
+void operator_add(const Storage *input1, float value, Storage *outputs)
+{
+    thrust::transform(input1->get_data().begin(), input1->get_data().end(),
+                      outputs->get_data().begin(), add_functor(value));
+}
+
+void operator_sub(const Storage *input1, const Storage *input2,
+                  Storage *outputs)
+{
+    CHECK_EQ(input1->get_data().size(), input2->get_data().size(),
+             "operator_sub: size error");
+
+    thrust::transform(input1->get_data().begin(), input1->get_data().end(),
+                      input2->get_data().begin(), outputs->get_data().begin(),
+                      thrust::minus<float>());
+}
