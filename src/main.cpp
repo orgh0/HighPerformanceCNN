b2fd6eb93a1b23628fdf604f106c63860ae496aa
Swapnil Pakhare <swapchamps@gmail.com>
2020-05-05 17:21:42 +0530

    Added code for testing model

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Dataset.hpp>
+#include<Model.hpp>
 #include <utils.hpp>
+
+#define BATCH_SIZE 128
+#define LEARNING_RATE 0.003
+#define L2 0.0001
+#define EPOCHS 30
+#define BETA 0.99
 
 int main() {
   DataSet dataset("./mnist_data", true);
@@ -8,4 +15,7 @@ int main() {
   auto cudaStatus = cudaSetDevice(0);
   CHECK_EQ(cudaStatus, cudaSuccess,
            "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+
+  Model mnist("./mnist_data", LEARNING_RATE, L2, BETA);
+  mnist.train(EPOCHS, BATCH_SIZE);
 }
